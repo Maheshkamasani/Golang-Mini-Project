e66ef16b04c425381af4e13e4ba63eb713071d3d
Mahesh <maheshkamasani1729@gmail.com>
2021-01-19 18:32:54 +0530

    Adding my mini project code

--- golang-blockchain/golang-blockchain/golang-blockchain/README.md
+++ golang-blockchain/golang-blockchain/golang-blockchain/README.md
@@ -1,10 +1,10 @@
 # Golang Blockchain
 
-### In this tutorial, we look at the go module system and build the basic framework for a blockchain. 
+### In this tutorial, we add transactions to our blockchain and we talk about inputs and outputs.
 
 ## Run `go run main.go` to run the app, run `go build main.go` to build an executable file.
 
-### Check out the Youtube Tutorial for this [Go Program](https://youtu.be/mYlHT9bB6OE). Here is our [Youtube Channel](https://www.youtube.com/channel/UCYqCZOwHbnPwyjawKfE21wg) Subscribe for more content.
+### Check out the Youtube Tutorial for this [Go Program](https://youtu.be/HNID8W2jgRM). Here is our [Youtube Channel](https://www.youtube.com/channel/UCYqCZOwHbnPwyjawKfE21wg) Subscribe for more content.
 
 ### Check out our blog at [tensor-programming.com](http://tensor-programming.com/).
 
---
+++ run.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+echo " Hello friends, I am mahesh, this is my Demo-----------------"
+sleep 5
+echo " Executing Main.go file.....pls wait..!!!"
+echo "    "
+echo "    "
+
+go run main.go
+sleep 30
+echo -n "Press y to continue"
+sleep 10
+cd ./golang-blockchain
+sleep 8
+echo " Executing Main.go file with random data2.....pls wait..!!!"
+echo "    "
+echo "    "
+echo "    "
+echo "    "
+
+go run main.go
+sleep 30
+echo -n "Press y to continue"
+sleep 10
+cd ./golang-blockchain
+sleep 8
+echo " Executing Main.go file with random data3.....pls wait..!!!"
+echo "    "
+echo "    "
+
+
+go run main.go add -block "First block"
+sleep 25
+echo " Printing new block chain just we added"
+go run main.go print
+
+echo -n "Press y to continue"
+sleep 10
+
+
+cd ./golang-blockchain
+sleep 8
+echo " Executing Main.go file with Transactions enableed  data4.....pls wait..!!!"
+echo "==================================================="
+go run main.go createblockchain -address "Mahesh"
+sleep 25
+echo " Printing new block chain just we added"
+go run main.go printchain
+sleep 15
+echo "Printing the balance of Mahesh"
+echo "===================================================="
+go run main.go getbalance -address Mahesh
+echo "Transferring balance/tokens from Chinna to Mahesh "
+echo "===================================================="
+go run main.go send -from Chinna -to Mahesh -amount 10 - Send
+echo "    "
+echo "    "
+echo "    "
+echo "    "
+echo "Printing the balance of Mahesh"
+echo "====================BALANCE================================"
+go run main.go getbalance -address Mahesh
+
+
+echo -n "Press y to continue"
+sleep 10
+
+cd ./golang-blockchain
+sleep 8
+echo " Executing Main.go file with Wallet module data5.....pls wait..!!!"
+echo "*********************WALLETS***********************************"
+go run main.go createwallet
+echo "   "
+go run main.go createwallet
+
+echo "    "
+echo "    "
+echo "    "
+echo "    "
+echo " listing the created address/wallets"
+echo "    "
+echo "    "
+go run main.go listaddresses
+sleep 10
+echo -n "Press y to continue"
+sleep 5
+echo " Executing Main.go file with Signature module enabled data5.....pls wait..!!!"
+echo "*********************SIGNATURES ENABLED***********************************"
+
+go run main.go createwallet
+echo "-------------------"
+go run main.go createwallet
+
+echo "            "
+
+echo " creating a new block chain with signature enabled"
+go run main.go createblockchain -address 14pgHNUConCHwAyKGBmRtdWpeP2PVuE18i
+echo " showing the block chain signatures"
+go run main.go printchain
+
+
+echo " Thank you.................!!!!!!!!!!!"
+
+sleep 3800
